29e2c559caa5e80401476fb918ba502239455df0
pinaki Saha <pinaki.saha86@gmail.com>
2022-03-12 12:07:50 +0530

    test

--- intro/variable_declaration.py
+++ intro/variable_declaration.py
@@ -107,7 +107,7 @@ def test():
     y = 'Local y'
     x = 'Local x'
     print(x + ', ' + y)  # prints 'Local x' and 'Local y'
-    print('hello  adasd 12324')
+    print('hello  adasd 123245')
 
 
 if __name__ == '__main__':
